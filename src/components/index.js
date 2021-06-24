aacab4706a31a7e1f03e520f7928a64ec67e5730
shravanmeena <shravanmeena47@gmail.com>
2021-06-24 16:55:36 +0530

    folder structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from "./Navbar/Navbar";
-export { default as InfoSection } from "./infoSection/InfoSection";
+export { default as InfoSection } from "./InfoSection/InfoSection";
 export { default as SecuritySection } from "./securitySection/SecuritySection";
